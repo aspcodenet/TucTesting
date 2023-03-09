936dfa788ede771dc903c046c3e7885ed8d7a8bb
aspcodenet <stefan.holmberg@systementor.se>
2023-03-09 09:46:19 +0100

    a

--- TucTesting.Tests/Services/RegistrationServiceTests.cs
+++ TucTesting.Tests/Services/RegistrationServiceTests.cs
@@ -3,10 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TucTesting.Models;
 using TucTesting.Services;
+using static TucTesting.Services.IRegistrationService;
 
 namespace TucTesting.Tests.Services
 {
+    public class FakeUserRepository : IUserRegistrationRepository
+    {
+        public List<string> registeredEmails = new List<string>();
+        public UserRegistration Get(string email)
+        {
+            throw new NotImplementedException();
+        }
+        public void CreateNew(string email)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
     // Är personens webaddress med domän @hej.se eller @hej.com ? Annars fail
     [TestClass]
     public class RegistrationServiceTests
@@ -17,6 +32,20 @@ namespace TucTesting.Tests.Services
         {
             sut = new RegistrationService(null,null);
         }
+
+        [TestMethod]
+        public void When_registering_and_already_registered_should_fail()
+        {
+            // ARRANGE
+            var userEmail = "stefan@hej.se";
+            // Få den att redan vara regostrerad
+            
+            //ACT
+            var result = sut.RegisterUser(userEmail);
+
+            //ASSERT
+            Assert.AreEqual(IRegistrationService.RegistrationStatus.AlreadyRegistered, result);
+        } 
 
         [TestMethod]
         public void When_registering_using_invalid_email_should_fail()
